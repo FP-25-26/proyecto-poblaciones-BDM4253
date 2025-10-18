11e53b94b39f89a95fc889dfbbf89a9f15257f31
BDM42532 <adrbejmon@alum.us.es>
2025-10-19 00:35:21 +0200

    1ra publicacion

--- src/poblacion_test.py
+++ src/poblacion_test.py
@@ -1,10 +1,40 @@
-from poblacion import leepoblaciones
-from poblacion import calcula_paises
-from poblacion import filtra_por_paises_y_anyo
+from poblacion import *
+
+def test_lee_poblaciones(ruta):
+    print("TESTEANDO: test_lee_poblaciones")
+    print(leepoblaciones(ruta))
+
+def test_calcula_paises(poblaciones):
+    print("TESTEANDO: test_calcula_paises")
+    print(calcula_paises(poblaciones))
+    
+def test_filtra_por_pais(poblaciones, nombre_o_codigo):
+    print("TESTEANDO: test_filtra_por_pais")
+    print(filtra_por_pais(poblaciones, nombre_o_codigo))
+    
+def test_filtra_por_paises_y_anyo(poblaciones, anyo, paises):
+    print("TESTEANDO: test_filtra_por_paises_y_anyo")
+    print(filtra_por_paises_y_anyo(poblaciones, anyo, paises))
+
+def test_muestra_evolucion_poblacion(poblaciones, nombre_o_codigo):
+    print("TESTEANDO: test_muestra_evolucion_poblacion")
+    muestra_evolucion_poblacion(poblaciones, nombre_o_codigo)
+    
+def test_muestra_comparativa_paises_anyo(poblaciones, anyo, paises):
+    print("TESTEANDO: test_muestra_comparativa_paises_anyo")
+    muestra_comparativa_paises_anyo(poblaciones, anyo, paises)
 
 def main():
-    paises = "Arab World", "Australia"
-    print (filtra_por_paises_y_anyo(leepoblaciones("./data/population.csv"), 1960, paises))
+    #test_lee_poblaciones("./data/population.csv")
+    #test_calcula_paises(leepoblaciones("./data/population.csv"))
+    #test_filtra_por_pais(leepoblaciones("./data/population.csv"), "Canada")
+    #test_filtra_por_pais(leepoblaciones("./data/population.csv"), "CAN")
+    #paises = ("Canada", "Cameroon")
+    #test_filtra_por_paises_y_anyo(leepoblaciones("./data/population.csv"), 1984, paises)
+    #test_muestra_evolucion_poblacion(leepoblaciones("./data/population.csv"), "Canada")
+    paises = ("Canada", "Cameroon")
+    test_muestra_comparativa_paises_anyo(leepoblaciones("./data/population.csv"), 1984, paises)
+    pass
 
 if __name__ == '__main__':
     main()
